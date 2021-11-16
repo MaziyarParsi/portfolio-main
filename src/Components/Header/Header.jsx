c4611783e1698da68933795e6c6d367b4995c687
maziyar parsi <mparsi1992@gmail.com>
2021-11-16 15:27:08 +0330

    edit-nav

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row,Col,Container,Nav } from 'react-bootstrap'
+import { Row,Col,Container,Nav,Navbar } from 'react-bootstrap'
 import Logo from '../../assets/Images/logo.png'
 import { Image } from 'react-bootstrap'
 import './Header.css'
@@ -8,24 +8,21 @@ const Header = () => {
  return (
  <div className="header-container  "> 
       <Container >
-      <Row  >
-        <Col className="d-flex align-items-center justify-content-start col-11  " >
-              <Nav activeKey="/home">
-            <Nav.Item className="me-2" >
-              <Nav.Link eventKey="link-2" className="nav-item-color">مهارت ها</Nav.Link>
-            </Nav.Item>
-            <Nav.Item className="me-2">
-              <Nav.Link eventKey="link-2" className="nav-item-color">خدمات</Nav.Link>
-            </Nav.Item>
-            <Nav.Item className="me-2 ">
-              <Nav.Link eventKey="link-1" className="nav-item-color">درباره من</Nav.Link>
-            </Nav.Item >
-            <Nav.Item className="" > 
-              <Nav.Link href="" className="nav-item-color">خانه</Nav.Link>
-            </Nav.Item>
-          </Nav>
+      <Row class="d-flex justify-content-start"  >
+        <Col className="col-11"  >
+        <Navbar  expand="lg"  >
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" >
+              <Nav className="ml-auto "  >
+                <Nav.Link className="nav-item-color me-4" href="#home">خانه</Nav.Link>
+                <Nav.Link className="nav-item-color me-4"  href="#link">درباه من</Nav.Link>
+                <Nav.Link className="nav-item-color me-4"  href="#home">خدمات</Nav.Link>
+                <Nav.Link className="nav-item-color me-4"  href="#link">مهارت ها</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
         </Col>
-        <Col className="col-1" >
+        <Col className="col-1 d-flex align-items-center" >
         <Image src={Logo} fluid ></Image>
         </Col>
       </Row>
